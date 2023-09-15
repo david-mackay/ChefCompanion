9099144e5f7da5ac2765b0aab1ecbc773978058e
david-mackay <davidmackay808@gmail.com>
2023-09-15 14:04:04 -0400

    recipes

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-console.log("JavaScript loaded!");
 let menuOpen = false;  // This variable keeps track of whether the menu is open or closed
 
 document.getElementById('menuButton').addEventListener('click', toggleMenu);
+
+function loadView(viewName) {
+    // Hide all sections first
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => section.style.display = 'none');
+
+    switch(viewName) {
+        case 'home':
+            document.getElementById('homeContent').style.display = 'block';
+            break;
+        case 'recipes':
+            document.getElementById('recipesContent').style.display = 'block';
+            // Optionally, you can call a function here to fetch and display recipes if they haven't been fetched yet.
+            break;
+        case 'pantry':
+            document.getElementById('pantryContent').style.display = 'block';
+            break;
+    }
+}
+
 
 function toggleMenuIcon(element) {
     element.classList.toggle('close');
@@ -34,10 +53,92 @@ function loadHomePage() {
     // fetchHomeData();
 }
 
-function loadRecipePage() {
-    // API call or DOM manipulation to load recipe page content
-    // fetchRecipeData();
+// ... existing JS ...
+// ... existing JS ...
+
+function fetchRecipeData() {
+    return new Promise((resolve) => {
+        // Simulating an async call with setTimeout.
+        // Replace this with your actual fetch/axios call in the future.
+        setTimeout(() => {
+            resolve([
+                {
+                    name: "Spaghetti Carbonara",
+                    description: "A classic Italian dish.",
+                    ingredients: ["Spaghetti", "Eggs", "Pancetta", "Parmesan cheese"],
+                    directions: ["Cook spaghetti.", "Mix eggs and cheese.", "Fry pancetta.", "Combine all ingredients."]
+                },
+                {
+                    name: "Chicken Tikka Masala",
+                    description: "A popular Indian dish.",
+                    ingredients: ["Chicken", "Yogurt", "Tomatoes", "Spices"],
+                    directions: ["Marinate chicken in yogurt and spices.", "Fry chicken.", "Make sauce with tomatoes.", "Combine chicken and sauce."]
+                },
+                {
+                    name: "Caesar Salad",
+                    description: "Healthy salad with romaine lettuce.",
+                    ingredients: ["Romaine lettuce", "Croutons", "Parmesan cheese", "Caesar dressing"],
+                    directions: ["Chop lettuce.", "Add croutons and cheese.", "Pour dressing."]
+                },
+                {
+                    name: "Chocolate Cake",
+                    description: "Decadent and moist chocolate dessert.",
+                    ingredients: ["Flour", "Cocoa powder", "Eggs", "Sugar"],
+                    directions: ["Mix dry ingredients.", "Add eggs.", "Bake in oven."]
+                }
+            ]);
+        }, 1000); // 1 second delay to simulate server response time.
+    });
 }
+
+function toggleRecipeDetails(clickedRecipeCard) {
+    // First, close any recipe card that's already expanded
+    const allRecipeCards = document.querySelectorAll('.recipe-card');
+    allRecipeCards.forEach(card => {
+        if (card !== clickedRecipeCard && card.classList.contains('expanded')) {
+            card.classList.remove('expanded');
+        }
+    });
+
+    // Now, toggle the expansion of the clicked recipe card
+    clickedRecipeCard.classList.toggle('expanded');
+}
+
+
+
+function loadRecipePage() {
+    const recipeContainer = document.getElementById('recipesContent');
+
+    fetchRecipeData().then(recipes => {
+        let recipeHtml = '';
+
+        recipes.forEach(recipe => {
+            recipeHtml += `
+            <div class="recipe-card" onclick="toggleRecipeDetails(this)">
+                <h3>${recipe.name}</h3>
+                <p class="recipe-description">${recipe.description}</p>
+                <div class="recipe-details">
+                    <h4>Ingredients:</h4>
+                    <ul class="recipe-ingredients">
+                        ${recipe.ingredients.map(ing => `<li>${ing}</li>`).join('')}
+                    </ul>
+                    <h4>Directions:</h4>
+                    <ul class="recipe-directions">
+                        ${recipe.directions.map(dir => `<li>${dir}</li>`).join('')}
+                    </ul>
+                </div>
+            </div>`;
+        });
+
+        recipeContainer.innerHTML = recipeHtml;
+        loadView('recipes');
+    });
+}
+
+// ... remaining JS ...
+
+// ... remaining JS ...
+
 
 function loadPantryPage() {
     // API call or DOM manipulation to load pantry page content
@@ -47,10 +148,6 @@ function loadPantryPage() {
 // Example API call functions (to be fleshed out)
 function fetchHomeData() {
     // API call to get home data
-}
-
-function fetchRecipeData() {
-    // API call to get recipe data
 }
 
 function fetchPantryData() {
@@ -77,4 +174,10 @@ function sendMessage() {
 
     // Clear the input field
     userMessageInput.value = '';
+}
+
+
+
+window.onload = function() {
+    loadView('home');
 }
